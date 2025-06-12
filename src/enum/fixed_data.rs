946b2d274778f365fbba70e64962e25f46cc111c
zsp19931222 <872126510@qq.com>
2025-06-12 16:24:58 +0800

    接口优化

--- src/enum/fixed_data.rs
+++ src/enum/fixed_data.rs
@@ -4,12 +4,15 @@ use serde::{Deserialize, Serialize};
 pub enum FixedDataConfig {
     #[serde(rename = "STRENGTH_NUMBERS")]
     StrengthNumbers,
+    #[serde(rename = "INTRODUCTION")]
+    Introduction,
 }
 
 impl FixedDataConfig {
     pub fn get_name(&self) -> &'static str {
         match self {
             FixedDataConfig::StrengthNumbers => "服务数",
+            FixedDataConfig::Introduction => "简介",
         }
     }
-} 
+}
